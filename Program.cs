e4afef32cf3e4386e23ddfc3f5eb3d557d59adbe
bakshi05 <devbakshi0524@gmail.com>
2026-07-19 00:08:41 +0530

    Initital Frontend commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+    using WebApplicationOfNationalParks.Repository;
+using WebApplicationOfNationalParks.Repository.IRepository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+
+// Register repositories (Dependency Injection)
+builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
+builder.Services.AddScoped<ITrailRepository, TrailRepository>();
+
+// Add HttpClient for API calls
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts(); // Default HSTS = 30 days
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Default route mapping
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ SD.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationOfNationalParks
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:5001/";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/NationalPark";
+        public static string TrailAPIPath = APIBaseUrl + "api/Trail";
+    }
+}
