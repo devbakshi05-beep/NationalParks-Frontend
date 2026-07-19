e64e29c4dab48dfacd57e1e10ce9676a853dac44
bakshi05 <devbakshi0524@gmail.com>
2026-07-19 17:41:50 +0530

    Update API base URL to live backend

--- SD.cs
+++ SD.cs
@@ -2,7 +2,7 @@
 {
     public static class SD
     {
-        public static string APIBaseUrl = "https://localhost:5001/";
+        public static string APIBaseUrl = "https://nationalparks-api-ms0q.onrender.com/";
         public static string NationalParkAPIPath = APIBaseUrl + "api/NationalPark";
         public static string TrailAPIPath = APIBaseUrl + "api/Trail";
     }
